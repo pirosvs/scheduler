e2c316479a496e7cfc4b744f20f8951e25e0b2db
darmstrong <armstrong2579@gmail.com>
2022-07-01 22:07:08 -0700

    add start for changing style of input forms based on time and clearing local storage after 1am

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Need to use an input form for text -- need to take the user input on event click submit button?
-// and set it in local storage.
-
-// Need to, upon initializing page, render local storage items?
-
-// Need label each form with the corresponding hour
-
-// Need to add style to boxes based upon time
-
-// indiv variable for each? Is there a way I can avoid doing this l;kajsdklfjsd and still have them work with
-// the time 
-
 // Displays the current date at the top of the page 
 var today = moment().format('dddd, MMM Do, YYYY');
 $("#currentDay").text(today);
@@ -32,26 +20,25 @@ var twentyThreeInfo = $('#twentyThreeNotes');
 var twentyFourInfo = $('#twentyFourNotes');
 var info = {};
 
+console.log(moment().hour());
+
+// Gets local storage as page initialized
 localStorage.getItem(info);
 
-var saveBtnEl = document.getElementsByClassName('saveBtn');
+var saveBtnEl = $('.saveBtn');
 
-var saveInfo = function(event) {
-    // error around preventDefault, which is not preventing the default??
-    // information is not even being taken in it looks like? console log shows nothing
-    // looks like button isn't working at all either, only can get action from pressing enter
-    // event.preventDefault();
-    info = {
-        tennotes : tenInfo.val(),
-    }    
-    localStorage.setItem("notes", JSON.stringify(info));
-    // localStorage.getItem("text");
-    console.log(info);
-    // needs to take from specific id and apply to that placeholder only
-    // var please = document.getElementById(nineNotes).input.val();
-    // placeholder.textContent = info;
-    // localStorage.setItem("info", info);
-};
+// var saveInfo = function(event) {
+//     // error around preventDefault, which is not preventing the default??
+//     // information is not even being taken in it looks like? console log shows nothing
+//     // looks like button isn't working at all either, only can get action from pressing enter
+//     // event.preventDefault();
+//     info = {
+//         tennotes : tenInfo.val(),
+//     }    
+//     localStorage.setItem("notes", JSON.stringify(info));
+//     // localStorage.getItem("text");
+//     console.log(info);
+// };
 
 // this set up will put it in the console log when enter is pressed instead of refreshing the page but doesn't save to
 // local storage and does not call the button what in the HELL is wrong with my buttons
@@ -64,3 +51,16 @@ $("button").click(function(event) {
     console.log(info);
 });
 
+// Changes style of form based on if the given time is already past, is in the future, or is the current hour
+if (moment().hour() == value of each form) {
+    form.addClass('present');
+} else if (moment().hour() > value) {
+    form.addClass('future');
+} else if (moment().hour() < value) {
+    form.addClass('past');
+};
+
+// Clears out storage after midnight
+if (moment().hour() == 1) {
+    localStorage.clear();
+}
