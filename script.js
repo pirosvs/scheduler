98c2994a4c3772864fc4156f1b11adb066081cdf
darmstrong <armstrong2579@gmail.com>
2022-07-01 20:52:24 -0700

    issue with local storage

--- script.js
+++ script.js
@@ -14,12 +14,48 @@
 var today = moment().format('dddd, MMM Do, YYYY');
 $("#currentDay").text(today);
 
+var nineInfo = document.getElementById("nineNotes");
+var tenInfo = $('#tenNotes');
+var elevenInfo = $("#elevenNotes");
+var twelveInfo = $("#twelveNotes");
+var thirteenInfo = $('#thirteenNotes');
+var fourteenInfo = $('#fourteenNotes');
+var fifteenInfo  = $('#fifteenNotes');
+var sixteenInfo = $('#sixteenNotes');
+var seventeenInfo = $('#seventeenNotes');
+var eighteenInfo = $('#eighteenNotes');
+var nineteenInfo = $('#nineteenNotes');
+var twentyInfo = $('#twentyNotes');
+var twentyOneInfo = $('#twentyOneNotes');
+var twentyTwoInfo = $('#twentyTwoNotes');
+var twentyThreeInfo = $('#twentyThreeNotes');
+var twentyFourInfo = $('#twentyFourNotes');
+var info = {};
+
+document.localStorage.getItem(info);
 
 var saveBtnEl = document.getElementsByClassName('saveBtn');
 
-var saveInfo = function() {
-    console.log("hello");
+var saveInfo = function(event) {
+    // error around preventDefault, which is not preventing the default??
+    // information is not even being taken in it looks like? console log shows nothing
+    // looks like button isn't working at all either, only can get action from pressing enter
+    event.preventDefault();
+    info = {
+        tennotes : tenInfo.value.trim(),
+    }    
+    localStorage.setItem("notes", JSON.stringify(info));
+    // localStorage.getItem("text");
+    console.log(info);
+    // needs to take from specific id and apply to that placeholder only
+    // var please = document.getElementById(nineNotes).input.val();
+    // placeholder.textContent = info;
+    // localStorage.setItem("info", info);
 };
 
-saveBtnEl.on('click', saveInfo);
+saveBtnEl.on('submit', saveInfo());
+
+function clickWork() {
+    console.log("click");
+}
 
